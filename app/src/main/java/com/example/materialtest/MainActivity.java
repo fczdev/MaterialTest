0dd5aabf1ff4acc5f50961e78de4bcca63ea70ef
fucunzhe <fcunzhe@gmail.com>
2017-03-05 22:06:44 +0800

    灏忔敼

--- app/src/main/java/com/example/materialtest/MainActivity.java
+++ app/src/main/java/com/example/materialtest/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     private List<Fruit> fruitList = new ArrayList<>();
 
     private FruitAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
